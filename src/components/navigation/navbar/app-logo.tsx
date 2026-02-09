45dbdd547b9de869343f6aac9e89024011bfa88c
serhii-afanasiev <frenkiefront@gmail.com>
2026-02-09 16:19:25 +0200

    Setup Dark & Light Themes

---
+++ src/components/navigation/navbar/app-logo.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function AppLogo() {
+  return (
+    <>
+      <Image
+        className="max-sm:size-5"
+        src="/images/site-logo.svg"
+        alt="DevOverflow Logo"
+        width={30}
+        height={30}
+        loading="eager"
+      />
+      <p className="heading-h2 max-sm:hidden">
+        <span className="text-dark-100 dark:text-light-900">Dev</span>
+        <span className="text-primary-500 font-medium">Overflow</span>
+      </p>
+    </>
+  );
+}
