218230f3fc86e2093385367db7be41ed5624ad5e
serhii-afanasiev <frenkiefront@gmail.com>
2026-02-10 18:27:39 +0200

    GitHub OAuth Setup. Added Snackbar component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+export default async function Home() {
+  const session = await auth();
+
+  console.log(session);
+
   return (
     <div className="flex min-h-screen items-center justify-center">
       <h1>PAGE</h1>
+
+      <form
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button>Log out</Button>
+      </form>
     </div>
   );
 }
--- src/components/forms/social-auth.tsx
+++ src/components/forms/social-auth.tsx
@@ -1,13 +1,39 @@
+"use client";
+
 import Image from "next/image";
+import { signIn } from "next-auth/react";
+import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 function SocialAuthForm() {
   const btnClasses = "max-sm:px-3.5 sm:flex-1";
 
+  //todo: Additional error handling after callback redirect from providers.
+  const handleSignIn = async (provider: "github" | "google") => {
+    try {
+      await signIn(provider, {
+        redirectTo: ROUTES.HOME,
+        // redirect: false,
+      });
+    } catch (e) {
+      console.log(e);
+
+      toast.error("Login failed", {
+        description: e instanceof Error ? e.message : "An error occurred during login",
+      });
+    }
+  };
+
   return (
     <div className="mt-9 flex flex-wrap gap-2.5 max-sm:justify-center">
-      <Button variant="social" size="lg" className={btnClasses}>
+      <Button
+        variant="social"
+        size="lg"
+        className={btnClasses}
+        onClick={() => handleSignIn("github")}
+      >
         <Image
           className="invert-colors"
           src="/icons/github.svg"
@@ -18,9 +44,14 @@ function SocialAuthForm() {
         <span className="max-sm:hidden">Login with GitHub</span>
       </Button>
 
-      <Button variant="social" size="lg" className={btnClasses}>
+      <Button
+        variant="social"
+        size="lg"
+        className={btnClasses}
+        onClick={() => handleSignIn("github")}
+      >
         <Image src="/icons/google.svg" width={20} height={20} alt="Google Logo" />
-        <span className="max-sm:hidden">Login with GitHub</span>
+        <span className="max-sm:hidden">Login with Google</span>
       </Button>
     </div>
   );
